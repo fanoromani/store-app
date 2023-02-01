f381ba8278437762f354f9ae859dca6012df0b6a
Stefano Romani <fanoromani@gmail.com>
2023-02-01 18:37:54 -0300

    Fonts applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: "Lato, sans-serif",
+        dosis: "Dosis, sans-serif",
+      },
       colors: {
         mainBG: "#DEDEDE",
       },
