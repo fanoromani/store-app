b68332bff7ea02bbe9474538261d028af158b8b3
Stefano Romani <fanoromani@gmail.com>
2023-02-03 18:46:27 -0300

    route setup

--- src/pages/product/[route]/index.tsx
+++ src/pages/product/[route]/index.tsx
@@ -1,27 +1,23 @@
+import { Header } from "@/components/Header";
 import Image from "next/image";
 
 export default function ProductPage() {
   return (
-    <div className="flex flex-col text-center items-center bg-white w-64 shadow-lg pb-5 pt-1">
-      <Image src="/wine.png" width={200} height={180} alt="Wine Bottle" />
-      <p className="font-bold text-base my-1">
-        Bacalhôa Meia Pipa Private Selection Castelão Syrah 2014
-      </p>
-      <div className="flex gap-1 items-center my-2">
-        <p className="font-bold text-gray-300 text-xs line-through">R$ 37,40</p>{" "}
-        <span className="text-fontSale bg-sale text-white rounded-sm py-[1px] px-[2px]">
-          60% OFF
-        </span>
+    <>
+      <Header />
+      <div className="max-w-[1440px] mt-10 flex mx-96">
+        <button className="flex items-center text-xl gap-5 text-gray-700">
+          <span>
+            <Image
+              src="/l_arrow.svg"
+              alt="Left Arrow"
+              width={12}
+              height={10}
+            ></Image>
+          </span>{" "}
+          Voltar
+        </button>
       </div>
-      <div className="flex gap-1 items-baseline mb-1">
-        <p className="text-gray-700 text-xs font-bold uppercase">Sócio wine</p>{" "}
-        <span className="text-pink-200 text-base font-bold">
-          R$ <span className="text-2xl">30</span>,00
-        </span>
-      </div>
-      <p className="text-gray-300 font-bold text-xs uppercase">
-        Não sócio R$ 37,40
-      </p>
-    </div>
+    </>
   );
 }
